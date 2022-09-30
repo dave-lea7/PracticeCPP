0526d92cb6042e00462edca72d9987422a05b624
krkzmain <kjsa777@gmail.com>
2022-09-30 16:51:21 +0900

    make cpp project

---
+++ Main.cpp
@@ -0,0 +1,20 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: 2022. 9. 30.
+ *      Author: keunholee
+ */
+
+#include "Main.h"
+
+Main::Main() 
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+Main::~Main() 
+{
+	// TODO Auto-generated destructor stub
+}
+
